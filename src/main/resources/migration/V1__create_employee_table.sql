5c1758404863bc3d5cbb1d4b78ac860a89ebd1a1
CForde-Kainos <connor.forde@kainos.com>
2024-10-15 15:45:36 +0100

    TJJ-019: Create Employee Table

---
+++ src/main/resources/migration/V1__create_employee_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Employee (
+	id int NOT NULL AUTO_INCREMENT,
+	firstName varchar(50),
+    lastName varchar(50),
+	salary decimal(7, 2),
+	bankAccountNumber varchar(34),
+	nationalInsuranceNumber varchar(9),
+	PRIMARY KEY(id)
+);
