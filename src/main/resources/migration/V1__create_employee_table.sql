c2b9df03c1f0516108ec2e693a1caeb32820fd3d
CForde-Kainos <connor.forde@kainos.com>
2024-10-15 15:54:06 +0100

    TJJ-019: Fixed spacing

--- src/main/resources/migration/V1__create_employee_table.sql
+++ src/main/resources/migration/V1__create_employee_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Employee (
 	id int NOT NULL AUTO_INCREMENT,
-	firstName varchar(50),
+    firstName varchar(50),
     lastName varchar(50),
 	salary decimal(7, 2),
 	bankAccountNumber varchar(34),
