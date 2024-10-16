c7257352e6e52bf6d330b0e84fe726957deaf2be
Matthew Jordan <matthew.jordan@kainos.com>
2024-10-16 11:23:19 +0100

    Defaulted workingOnProject to True.
    Employees are only ever added to this table when they are assigned to a project,
    so I think it's safe to assume this column should default to true.

--- src/main/resources/db/migration/V7__create_delivery_employee_project_table.sql
+++ src/main/resources/db/migration/V7__create_delivery_employee_project_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE DeliveryEmployeeProject (
 	deliveryEmployeeId int NOT NULL,
 	projectId int NOT NULL,
-	workingOnProject BOOL,
+	workingOnProject BOOL NOT NULL DEFAULT TRUE,
 	PRIMARY KEY(deliveryEmployeeId, projectId),
 	FOREIGN KEY (deliveryEmployeeId) REFERENCES DeliveryEmployee(employeeId),
 	FOREIGN KEY (projectId) REFERENCES Project(projectId)
