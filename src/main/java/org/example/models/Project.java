2161b914af5a4af9bcced655be156e11c5031ddc
dylanmckk <dylan.mckenzie@kainos.com>
2024-10-16 13:59:24 +0100

    TJJ-028: Reverted to previous declaration of projectDate

--- src/main/java/org/example/models/Project.java
+++ src/main/java/org/example/models/Project.java
@@ -9,7 +9,7 @@ public class Project {
     private int clientId;
     private int techLeadId;
     private int salesEmployeeId;
-    private final ProjectDate projectDate;
+    private ProjectDate projectDate;
 
     public Project(final int projectId,
                    final String name,
