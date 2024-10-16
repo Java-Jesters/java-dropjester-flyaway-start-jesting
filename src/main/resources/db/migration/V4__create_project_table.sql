51d34c1bf7510130b00073ae46ff1f85c1f0009c
dylanmckk <dylan.mckenzie@kainos.com>
2024-10-16 10:02:20 +0100

    TJJ-005: Fixed naming (file, IDs and value)

---
+++ src/main/resources/db/migration/V4__create_project_table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Project (
+	projectId int NOT NULL AUTO_INCREMENT,
+	`name` varchar(50) NOT NULL,
+	projectValue decimal(11, 2) NOT NULL,
+	clientId int NOT NULL,
+	techLeadId int NOT NULL,
+	salesEmployeeId int NOT NULL,
+	startDate DateTime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	finishDate DateTime,
+	PRIMARY KEY(projectId),
+	FOREIGN KEY (clientId) REFERENCES Client(clientId),
+	FOREIGN KEY (techLeadId) REFERENCES DeliveryEmployee(deliveryEmployeeId),
+	FOREIGN KEY (salesEmployeeId) REFERENCES SalesEmployee(salesEmployeeId)
+);
