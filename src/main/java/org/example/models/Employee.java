c07e630f0be0cf8ba416eeaa424d7ba2f7a104e1
Matthew Jordan <matthew.jordan@kainos.com>
2024-10-16 13:09:04 +0100

    TJJ-027: Fixed linting errors

--- src/main/java/org/example/models/Employee.java
+++ src/main/java/org/example/models/Employee.java
@@ -1,58 +1,64 @@
 package org.example.models;
 
 public class Employee {
-	private int employeeId;
-	private String name;
-	private double salary;
-	private String bankAccountNumber;
-	private String nationalInsuranceNumber;
+    private int employeeId;
+    private String name;
+    private double salary;
+    private String bankAccountNumber;
+    private String nationalInsuranceNumber;
 
-	public Employee(int employeeId, String name, double salary,
-					String bankAccountNumber, String nationalInsuranceNumber) {
-		this.employeeId = employeeId;
-		this.name = name;
-		this.salary = salary;
-		this.bankAccountNumber = bankAccountNumber;
-		this.nationalInsuranceNumber = nationalInsuranceNumber;
-	}
+    public Employee(final int employeeId,
+                    final String name,
+                    final double salary,
+                    final String bankAccountNumber,
+                    final String nationalInsuranceNumber
+    ) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.salary = salary;
+        this.bankAccountNumber = bankAccountNumber;
+        this.nationalInsuranceNumber = nationalInsuranceNumber;
+    }
 
-	public int getEmployeeId() {
-		return employeeId;
-	}
+    public int getEmployeeId() {
+        return employeeId;
+    }
 
-	public void setEmployeeId(int employeeId) {
-		this.employeeId = employeeId;
-	}
+    public void setEmployeeId(final int employeeId) {
+        this.employeeId = employeeId;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(final String name) {
+        this.name = name;
+    }
 
-	public double getSalary() {
-		return salary;
-	}
+    public double getSalary() {
+        return salary;
+    }
 
-	public void setSalary(double salary) {
-		this.salary = salary;
-	}
+    public void setSalary(final double salary) {
+        this.salary = salary;
+    }
 
-	public String getBankAccountNumber() {
-		return bankAccountNumber;
-	}
+    public String getBankAccountNumber() {
+        return bankAccountNumber;
+    }
 
-	public void setBankAccountNumber(String bankAccountNumber) {
-		this.bankAccountNumber = bankAccountNumber;
-	}
+    public void setBankAccountNumber(final String bankAccountNumber) {
+        this.bankAccountNumber = bankAccountNumber;
+    }
 
-	public String getNationalInsuranceNumber() {
-		return nationalInsuranceNumber;
-	}
+    public String getNationalInsuranceNumber() {
+        return nationalInsuranceNumber;
+    }
 
-	public void setNationalInsuranceNumber(String nationalInsuranceNumber) {
-		this.nationalInsuranceNumber = nationalInsuranceNumber;
-	}
+    public void setNationalInsuranceNumber(
+            final String nationalInsuranceNumber
+    ) {
+        this.nationalInsuranceNumber = nationalInsuranceNumber;
+    }
 }
