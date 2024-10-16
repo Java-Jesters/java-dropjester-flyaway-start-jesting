53ae2a9d275f83f6fccc6d58ef263456b21e37d9
Matthew Jordan <matthew.jordan@kainos.com>
2024-10-16 15:34:11 +0100

    TJJ-002: Exceptions now inherit from the Exception class

--- src/main/java/org/example/exceptions/FailedToCreateException.java
+++ src/main/java/org/example/exceptions/FailedToCreateException.java
@@ -1,6 +1,6 @@
 package org.example.exceptions;
 
-public class FailedToCreateException extends Throwable {
+public class FailedToCreateException extends Exception {
     public FailedToCreateException(final Entity entity) {
         super("Failed to create " + entity.getEntity());
     }
