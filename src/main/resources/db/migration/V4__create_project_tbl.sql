355575ddbfb316e8deba2b08599984e1f3c5dd1e
dylanmckk <dylan.mckenzie@kainos.com>
2024-10-16 09:18:11 +0100

    TJJ-005: Fixed tabbing

--- src/main/resources/db/migration/V4__create_project_tbl.sql
+++ src/main/resources/db/migration/V4__create_project_tbl.sql
@@ -1,14 +1,14 @@
 CREATE TABLE Project (
-    id int NOT NULL AUTO_INCREMENT,
-    `name` varchar(50) NOT NULL,
-    `value` decimal(11, 2) NOT NULL,
-    clientId int NOT NULL,
-    techLeadId int NOT NULL,
-    salesEmployeeId int NOT NULL,
-    startDate DateTime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    finishDate DateTime,
-    PRIMARY KEY(id),
-    FOREIGN KEY (clientId) REFERENCES Client(id),
-    FOREIGN KEY (techLeadId) REFERENCES DeliveryEmployee(id),
-    FOREIGN KEY (salesEmployeeId) REFERENCES SalesEmployee(id)
+	id int NOT NULL AUTO_INCREMENT,
+	`name` varchar(50) NOT NULL,
+	`value` decimal(11, 2) NOT NULL,
+	clientId int NOT NULL,
+	techLeadId int NOT NULL,
+	salesEmployeeId int NOT NULL,
+	startDate DateTime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	finishDate DateTime,
+	PRIMARY KEY(id),
+	FOREIGN KEY (clientId) REFERENCES Client(id),
+	FOREIGN KEY (techLeadId) REFERENCES DeliveryEmployee(id),
+	FOREIGN KEY (salesEmployeeId) REFERENCES SalesEmployee(id)
 );
