943c2711c9ebe7fe19be8ffc932d0382f05e8827
dylanmckk <dylan.mckenzie@kainos.com>
2024-10-16 09:37:11 +0100

    TJJ-021: Added primary keys

--- src/main/resources/db/migration/V5__create_deliv_emp_proj_tbl.sql
+++ src/main/resources/db/migration/V5__create_deliv_emp_proj_tbl.sql
@@ -2,6 +2,7 @@ CREATE TABLE DeliveryEmployeeProject (
 	deliveryEmployeeId int NOT NULL,
 	projectId int NOT NULL,
 	workingOnProject TINYINT(1),
+	PRIMARY KEY(delivery_employee_id, working_on_project),
 	FOREIGN KEY (deliveryEmployeeId) REFERENCES DeliveryEmployee(id),
 	FOREIGN KEY (projectId) REFERENCES Project(id)
 );
