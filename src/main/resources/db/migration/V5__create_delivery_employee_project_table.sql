2b9cdbe04521f748728e77b70e7adaf7144cae62
dylanmckk <dylan.mckenzie@kainos.com>
2024-10-16 10:21:19 +0100

    TJJ-021: Fixed naming of IDs, changed Tinyint(1) to BOOL, and updated file name for migration purposes

--- src/main/resources/db/migration/V5__create_delivery_employee_project_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE DeliveryEmployeeProject (
-	deliveryEmployeeId int NOT NULL,
-	projectId int NOT NULL,
-	workingOnProject TINYINT(1),
-	PRIMARY KEY(delivery_employee_id, working_on_project),
-	FOREIGN KEY (deliveryEmployeeId) REFERENCES DeliveryEmployee(id),
-	FOREIGN KEY (projectId) REFERENCES Project(id)
-);
