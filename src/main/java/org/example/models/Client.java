45353580884ca0aa43138b3f274800fcce6c80c4
dylanmckk <dylan.mckenzie@kainos.com>
2024-10-16 15:03:07 +0100

    TJJ-030: Removed getters and setters of client address in client model as already existed in the client address model

--- src/main/java/org/example/models/Client.java
+++ src/main/java/org/example/models/Client.java
@@ -17,6 +17,10 @@ public class Client {
         this.clientAddress = clientAddress;
     }
 
+    public void setClientAddress(final ClientAddress clientAddress) {
+        this.clientAddress = clientAddress;
+    }
+
     public int getClientId() {
         return clientId;
     }
@@ -39,61 +43,5 @@ public class Client {
 
     public void setPhoneNumber(final int phoneNumber) {
         this.phoneNumber = phoneNumber;
-    }
-
-    public String getAddressLine1() {
-        return clientAddress.getAddressLine1();
-    }
-
-    public void setAddressLine1(final String addressLine1) {
-        this.clientAddress.setAddressLine1(addressLine1);
-    }
-
-    public String getAddressLine2() {
-        return clientAddress.getAddressLine2();
-    }
-
-    public void setAddressLine2(final String addressLine2) {
-        this.clientAddress.setAddressLine2(addressLine2);
-    }
-
-    public String getAddressCity() {
-        return clientAddress.getAddressCity();
-    }
-
-    public void setAddressCity(final String addressCity) {
-        this.clientAddress.setAddressCity(addressCity);
-    }
-
-    public String getAddressProvince() {
-        return clientAddress.getAddressProvince();
-    }
-
-    public void setAddressProvince(final String addressProvince) {
-        this.clientAddress.setAddressProvince(addressProvince);
-    }
-
-    public String getAddressRegion() {
-        return clientAddress.getAddressRegion();
-    }
-
-    public void setAddressRegion(final String addressRegion) {
-        this.clientAddress.setAddressRegion(addressRegion);
-    }
-
-    public String getAddressCountry() {
-        return clientAddress.getAddressCountry();
-    }
-
-    public void setAddressCountry(final String addressCountry) {
-        this.clientAddress.setAddressCountry(addressCountry);
-    }
-
-    public String getAddressPostalCode() {
-        return clientAddress.getPostalCode();
-    }
-
-    public void setAddressPostalCode(final String postalCode) {
-        this.clientAddress.setPostalCode(postalCode);
     }
 }
