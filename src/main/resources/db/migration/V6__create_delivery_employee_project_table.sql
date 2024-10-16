87fc9ef83f33b83d155168bcb350488cf4162cbd
dylanmckk <dylan.mckenzie@kainos.com>
2024-10-16 10:21:57 +0100

    TJJ-021: Fixed tabbing

--- src/main/resources/db/migration/V6__create_delivery_employee_project_table.sql
+++ src/main/resources/db/migration/V6__create_delivery_employee_project_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE DeliveryEmployeeProject (
-deliveryEmployeeId int NOT NULL,
-projectId int NOT NULL,
-workingOnProject BOOL,
-FOREIGN KEY (deliveryEmployeeId) REFERENCES DeliveryEmployee(deliveryEmployeeId),
-FOREIGN KEY (projectId) REFERENCES Project(projectId)
+	deliveryEmployeeId int NOT NULL,
+	projectId int NOT NULL,
+	workingOnProject BOOL,
+	FOREIGN KEY (deliveryEmployeeId) REFERENCES DeliveryEmployee(deliveryEmployeeId),
+	FOREIGN KEY (projectId) REFERENCES Project(projectId)
 );
